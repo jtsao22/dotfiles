cfba768bf1e6b705e8707d45ff88cbf81a715675
Jason Tsao <jtsao@lab126.com>
2014-12-23 14:13:43 -0800

    Removed indentLines, added bash_it updates, added DejaVuSans as default font for terminator, and show tabs in vim.

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -3,6 +3,7 @@ sudo apt-get install meld git vim awesome synergy terminator
 # Linux
 # Zeal
 # Sublime Text
+# DejaVu fonts (http://sourceforge.net/projects/dejavu/)
 
 # Mac coreutils includes ls & grep
 #brew install coreutils
