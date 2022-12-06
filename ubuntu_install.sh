c3de407bae6a7ed17ac3f6265987c0a786e3b42f
Jason Tsao <jtsao@lab126.com>
2022-12-06 17:53:17 +0100

    Updated i3 repo to more recent build

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -45,7 +45,7 @@ if [ -z "$no_gui" ]; then
 	#gtk-icon-theme-name = Faenza-Dark
 
 	# Install latest i3 from https://i3wm.org/docs/repositories.html
-	/usr/lib/apt/apt-helper download-file https://debian.sur4r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2021.02.02_all.deb keyring.deb SHA256:cccfb1dd7d6b1b6a137bb96ea5b5eef18a0a4a6df1d6c0c37832025d2edaa710
+    /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2022.02.17_all.deb keyring.deb SHA256:52053550c4ecb4e97c48900c61b2df4ec50728249d054190e8a0925addb12fc6
 	sudo dpkg -i ./keyring.deb
 	echo "deb http://debian.sur4r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
 	sudo apt update
