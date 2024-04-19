6620c96b55efbf775f6ddca0810559f2a357cf2f
Jason Tsao <jtsao@lab126.com>
2024-04-19 18:10:27 +0200

    Added some mac settings for quicker dock action and using jpg for screenshots

--- mac_install.sh
+++ mac_install.sh
@@ -1,3 +1,14 @@
 brew install rg fd fzf exa neovim bash-completion bat autojump
 
 # Reminder to install alfred workflows in alfred folder
+
+# Make dock showup instantly and go away instantly
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0
+killall Dock
+
+# Use shottr for screenshots, but if must use mac's, use jpg
+defaults write com.apple.screencapture type jpg
+
+# Make dock icons grey if it's hidden
+defaults write com.apple.Dock showhidden -bool TRUE && killall Dock
