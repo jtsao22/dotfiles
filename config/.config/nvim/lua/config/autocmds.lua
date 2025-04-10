24e5927ab15c8b85cee7ab22f9856c6948a27b87
Jason Tsao <jtsao@lab126.com>
2025-04-10 15:06:53 +0200

    Add autocmd for js-like languages for using 2 spaces

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Use 2 spaces for javascript-like languages
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "typescript", "javascript", "typescriptreact", "javascriptreact", "lua" },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.expandtab = true
+  end,
+})
