a977bbae96dcd1dd578191eb4836d800288d6c45
Jason Tsao <jtsao@lab126.com>
2025-04-27 15:23:32 +0200

    Add rust lsp external docs keymap

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open external docs for LSP (useful for rust in particular)
+local map = vim.keymap.set
+map("n", "<leader>k", function()
+  vim.cmd("RustLsp externalDocs")
+end, { desc = "Show Rust LSP external docs" })
